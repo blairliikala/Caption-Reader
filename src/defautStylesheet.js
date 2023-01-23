4185de251f0d5b56d7147bd642ed75e76c815736
Blair <blair.liikala@gmail.com>
2023-01-22 18:55:02 -0600

    Update defautStylesheet.js

--- src/defautStylesheet.js
+++ src/defautStylesheet.js
@@ -1,169 +1,151 @@
 export function defaultStyles() {
   return `<style>
-    #root {
-      display: block;
-      scroll-behavior: smooth;
-      height: 300px;
-      overflow-y: scroll;
-      overflow-x: hidden;
-      scroll-snap-stop: always;
-      position: relative;
-      padding: .5rem;
-      --base: 360;
-    }
-    #root * {
-      box-sizing: border-box;
-    }
-    .empty {
-      color: hsla(var(--base), 20%, 40%, .9);
-    }
-    ol {
-      padding: 0;
-      margin: 0;
-    }
-    li {
-      list-style:none;
-      width: 100%;
-    }
-    .cue {
-      border: none;
-      font: inherit;
-      padding: 0.4rem 1.5rem .4rem .2rem;
-      display: flex;
-      gap: 1rem;
-      list-style: none;
-      transform: scale(1);
-      transform-origin: left;
-      color: hsla(var(--base), 20%, 40%, .9);
-      transition: color 0.3s ease, font-size .2s ease, transform .1s ease;
-      background: none;
-      width: 100%;
-      text-align: start;
-    }
-    @media(hover: hover) and (pointer: fine) {
-      .cue:hover, .cue:active {
-        cursor: pointer;
-        background: hsla(var(--base), 60%, 70%, .1);
-        outline: 1px solid hsla(var(--base), 60%, 50%, .1);
-        color: hsla(var(--base), 10%, 20%, 1);
-      }
-    }
-    .cue:focus-visible {
+
+  #root {
+    display: block;
+    scroll-behavior: smooth;
+    height: 300px;
+    overflow-y: scroll;
+    overflow-x: hidden;
+    scroll-snap-stop: always;
+    position: relative;
+    padding: .5rem;
+    --base: 360;
+    
+    --gray-10-sat: 10%;
+    --gray-10-light: 10%;
+    --gray-10-opacity: 0.1;
+    --gray-20-sat: 10%;
+    --gray-20-light: 10%;
+    --gray-20-opacity: 0.2;
+    --gray-70-opacity: 0.7;
+    --inactive: hsla(var(--base), 20%, 40%, 0.9);
+    --active-primary: hsla(var(--base), 0%, 30%, 1);
+    --active-secondary: hsla(var(--base), 20%, 80%, 1);
+    --highlight: hsla(var(--base), 50%, 50%, 0.9);
+  }
+  #root * {
+    box-sizing: border-box;
+  }
+  
+  [data-theme="dark"]#root {
+    --gray-10-sat: 10%;
+    --gray-10-light: 10%;
+    --gray-10-opacity: 0.1;
+    --gray-20-sat: 10%;
+    --gray-20-light: 10%;
+    --gray-20-opacity: 0.2;
+    --gray-70-opacity: 0.7;
+    --inactive: hsla(var(--base), 10%, 80%, 0.7); 
+    --active-primary: hsla(var(--base), 0%, 100%, 1);
+    --active-secondary: hsla(var(--base), 20%, 80%, 1);
+    --highlight: hsla(var(--base), 50%, 60%, 0.9);
+  }
+  
+  .empty {
+    color: hsla(var(--base), var(--gray-10-sat), var(--gray-10-light), var(--gray-70-opacity));
+  }
+  ol {
+    padding: 0;
+    margin: 0;
+  }
+  li {
+    list-style:none;
+    width: 100%;
+  }
+  .cue {
+    border: none;
+    font: inherit;
+    padding: 0.3rem 1.5rem 0.3rem .4rem;
+    display: flex;
+    gap: 1rem;
+    transform: scale(1);
+    transform-origin: left;
+    color: var(--inactive);
+    transition: all .3s ease;
+    background: none;
+    width: 100%;
+    text-align: start;
+    border-left: 2px solid hsla(var(--base), 60%, 70%, 0.1);
+    border-radius: 0;
+  }
+  @media(hover: hover) and (pointer: fine) {
+    .cue:hover, .cue:active, .cue:focus {
       cursor: pointer;
-      background: hsla(var(--base), 60%, 70%, .1);
-      outline: 1px solid hsla(var(--base), 60%, 50%, .1);
-      color: hsla(var(--base), 10%, 20%, 1);
-    }    
-    .upcoming, .upcoming:focus-visable {
-      color: hsla(var(--base), 20%, 60%, .7);
-      transform: scale(1);
-      transform-origin: left;
+      color: var(--active-secondary);
+      background: none;
+      border-left-color: var(--highlight);
+      outline: 1px solid var(--highlight);
     }
-    .next, .next:focus-visable {
-      color: hsla(var(--base), 20%, 40%, .9);
-      transform: scale(1);
-      transform-origin: left;
+  }
+  .cue:focus-visible {
+    cursor: pointer;
+    border-left-color: var(--highlight);
+    outline: 1px solid var(--highlight);
+    color: var(--highlight);
+    background: none;
+  }    
+  .upcoming {
+    transform: scale(1);
+    transform-origin: left;
+  }
+  .next {
+    transform: scale(1);
+    transform-origin: left;
+  }
+  .active {
+    transform: scale(1.05);
+    transform-origin: left;
+    padding-right: 10%;
+    border-color: var(--highlight);
+    font-weight: bold;
+  }
+  .active .timecode {
+    color: var(--active-secondary); 
+  }
+  .active .chapter {
+    color: var(--active-secondary);  
+  }
+  .active .text  {
+    color: var(--active-primary);
+  }
+  .passed  {
+    transform: scale(1);
+    transform-origin: left;
+  }
+  
+  @media (prefers-reduced-motion) {
+    .active, .previous {
+      font-size: unset;
     }
-    .active {
-      transform: scale(1.1);
-      transform-origin: left;
-      padding-right: 10%;
-    }
-      .active .timecode, .active .chapter {
-        color: hsla(var(--base), 50%, 30%, 1);
-      }
-      .active .text, .active .chapter {
-        color: hsla(var(--base), 0%, 30%, 1);
-        font-weight: bold;
-      }
-    .passed, .passed:focus-visable  {
-      color: hsla(var(--base), 20%, 60%, .7);
-      transform: scale(1);
-      transform-origin: left;
-    }
-    .spacer .text {
-      color: hsla(var(--base), 20%, 60%, .5);
-      letter-spacing: 5px;
-      font-weight: bold;
-    }
-
-    [data-theme="dark"] .empty {
-      color: hsla(var(--base), 30%, 80%, .9);
-    }
-    [data-theme="dark"] .cue {
-      color: hsla(var(--base), 10%, 80%, .7);
-    }
-    @media(hover: hover) and (pointer: fine) {
-      [data-theme="dark"] .cue:hover,
-      [data-theme="dark"] .cue:active,
-      [data-theme="dark"] .cue:focus {
-        background: hsla(var(--base), 60%, 70%, .1);
-        outline: 1px solid hsla(var(--base), 60%, 50%, .1);
-        color: hsla(var(--base), 0%, 100%, 1);
-      }
-    }
-    [data-theme="dark"] .cue:focus-visible {
-      background: hsla(var(--base), 60%, 70%, .1);
-      outline: 1px solid hsla(var(--base), 60%, 50%, .1);
-      color: hsla(var(--base), 0%, 100%, 1);
-    }
-    [data-theme="dark"] .upcoming, .upcoming:focus-visable {
-      color: hsla(var(--base), 10%, 80%, .7);
-    }
-    [data-theme="dark"] .next, .next:focus-visable {
-      color: hsla(var(--base), 20%, 80%, .9);
-    }
-    [data-theme="dark"] .active .timecode,
-    [data-theme="dark"] .active .chapter
-    {
-      color: hsla(var(--base), 50%, 80%, 1);
-    }
-    [data-theme="dark"] .active .text,
-    [data-theme="dark"] .active .chapter
-    {
-      color: hsla(var(--base), 0%, 100%, 1);
-    }
-
-    [data-theme="dark"] .previous, .previous:focus-visable {
-      color: hsla(var(--base), 20%, 80%, .9);
-    }
-    [data-theme="dark"] .passed, .passed:focus-visable {
-      color: hsla(var(--base), 10%, 80%, .7);
-    }
-
-    @media (prefers-reduced-motion) {
-      .active, .previous {
-        font-size: unset;
-      }
-    }
-
-    progress {
-      appearance: none;
-      background: hsla(var(--base), 10%, 10%, .1);
-      border: none;
-      border-radius: 2px;
-      height: 8px;
-      align-self: center;
-    }
-
-    progress[value]::-webkit-progress-bar {
-      background: hsla(var(--base), 10%, 10%, .2);
-      box-shadow: none;
-    }
-
-    progress[value]::-moz-progress-bar {
-      background: hsla(var(--base), 10%, 10%, .2);
-      box-shadow: none;
-    }
-    progress[value]::-webkit-progress-value {
-      background: hsla(var(--base), 10%, 10%, .2);
-      box-shadow: none;
-    }
-
-    .active .sub_upcoming {
-    }
-    .active .sub_active {
-      text-decoration: underline;
-    }
+  }
+  
+  progress {
+    appearance: none;
+    background: hsla(var(--base), var(--gray-10-sat), var(--gray-10-light), var(--gray-10-opacity));
+    border: none;
+    border-radius: 2px;
+    height: 8px;
+    align-self: center;
+  }
+  
+  progress[value]::-webkit-progress-bar {
+    background: hsla(var(--base), var(--gray-20-sat), var(--gray-20-light), var(--gray-20-opacity));
+    box-shadow: none;
+  }
+  
+  progress[value]::-moz-progress-bar {
+    background: hsla(var(--base), var(--gray-20-sat), var(--gray-20-light), var(--gray-20-opacity));
+    box-shadow: none;
+  }
+  progress[value]::-webkit-progress-value {
+    background: hsla(var(--base), var(--gray-20-sat), var(--gray-20-light), var(--gray-20-opacity));
+    box-shadow: none;
+  }
+  
+  .active .sub_active {
+    text-decoration: underline;
+  }
+  
   </style>`;
 }
